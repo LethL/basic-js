3b98b9e06b1734eff2f95a67f1dc4457b664c22c
Leth4L <carribean_08@mail.ru>
2021-03-05 14:23:11 +0300

    feat: add transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+
+  let array = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] === '--discard-next') {
+      array.push('empty el');
+    } else if (arr[i + 1] === '--discard-prev') {
+      array.push('empty el');
+    } else if (arr[i] === '--double-next') {
+      array.push(arr[i + 1])
+    } else if (arr[i] === '--double-prev') {
+      array.push(array[array.length - 1])
+    } else {
+      array.push(arr[i])
+    }
+  }
+
+  let complete = array.filter(el => 
+  el != "empty el" &&
+  el != "--discard-next" &&
+  el != "--discard-prev" &&
+  el != undefined
+  );
+
+  return complete;
 };
