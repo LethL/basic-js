784c8d50e403dac0f11142c6b172cb07fcb00bf6
Leth4L <carribean_08@mail.ru>
2021-03-06 13:19:06 +0300

    feat: add dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = '';
+
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  members.forEach(function(el) {
+    if (typeof el === 'string') {
+      dreamTeam += el.trim().slice(0,1).toUpperCase();
+    }
+  });
+
+  return dreamTeam.split('').sort().join('');
 };
