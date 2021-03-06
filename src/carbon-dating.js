0bd81045854cad33fbe1c8b6bdf393827f8c07b1
Leth4L <carribean_08@mail.ru>
2021-03-06 14:36:02 +0300

    fix: add carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (!sampleActivity || typeof +sampleActivity === 'number' || typeof sampleActivity !== 'string') {
+  if (!sampleActivity || typeof +sampleActivity === 'number' || typeof sampleActivity !== 'string' 
+  || sampleActivityNumber <= 0 || sampleActivityNumber > 15) {
     return false
   }
 
